06ce9d58df8aacb053e9f3603a2e23f68d599dba
Jonnas Costa <jonn567@gmail.com>
2022-08-06 18:00:17 +0100

    enable more layouts

--- config.py
+++ config.py
@@ -112,8 +112,8 @@ layouts = [
     layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
     layout.Max(),
     # Try more layouts by unleashing below layouts.
-    # layout.Stack(num_stacks=2),
-    # layout.Bsp(),
+    layout.Stack(num_stacks=3),
+    layout.Bsp(),
     # layout.Matrix(),
     # layout.MonadTall(),
     # layout.MonadWide(),
